fb105b63c0e811f80e3b88dbb9754659f88335fc
Hamelinny <anastasiyasofr@yandex.ru>
2016-10-04 21:23:36 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ static void qemu_gdb_hang(void)
 #include <interrupt.h>
 #include <timer.h>
 #include <ioport.h>
+#include <io.h>
 
 extern uint64_t handler[];
 
@@ -29,10 +30,20 @@ void main(void)
 	init_idt();
 	init_pit();
 
-    enable_ints();
+        enable_ints();
 	
 	__asm__("int $40");
 
+	printf("%d\n", 10);
+	printf("%ho\n", 8);
+	printf("%hhx\n", 16);
+	printf("%x\n", 255);
+	char a[10];
+	char * x = a;
+	printf("%s\n", "hello");
+	printf("%d\n", snprintf(x, 10, "%s", "hello"));
+	printf("%s\n", a);
+	printf("%lld\n", (1ll << 62));
 	
 	while (1);
 	out8(0x21, (1 << 8) - 1);
