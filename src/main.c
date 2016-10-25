dce86a6d2fee50a1096ba3019a6d327509d95abe
Hamelinny <anastasiyasofr@yandex.ru>
2016-10-25 16:11:34 +0300

    new main

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 static void qemu_gdb_hang(void)
 {
 #ifdef DEBUG
-	static volatile int wait = 1;
+    static volatile int wait = 1;
 
-	while (wait);
+    while (wait);
 #endif
 }
 
@@ -16,47 +16,59 @@ static void qemu_gdb_hang(void)
 #include <io.h>
 #include <memmap.h>
 #include <buddy.h>
+#include <slab.h>
 
 extern uint64_t handler[];
 
-void main(void)
-{
-	qemu_gdb_hang();
-	disable_ints();
-	init_serial();
+void main(void) {
+    qemu_gdb_hang();
+    disable_ints();
+    init_serial();
 
 
-	print_string("serial port");
-	
-	init_master_and_slave();
-	init_idt();
-	init_pit();
+    print_string("serial port");
+    
+    init_master_and_slave();
+    init_idt();
+    init_pit();
 
-        enable_ints();
-	
-	//__asm__("int $40");
+    enable_ints();
+    
+    //__asm__("int $40");
 
-	disable_ints();
+    disable_ints();
 
-	get_memmap();
-	print_memmap();
-	/*uint64_t mem = buddy_alloc(0);
-	printf("%x\n", mem);
-	buddy_free(mem);
-	mem = buddy_alloc(1);
-	printf("%x\n", mem);
-	buddy_free(mem);
-	mem = buddy_alloc(2);
-	printf("%x\n", mem);
-	buddy_free(mem);
-	uint64_t mem1 = buddy_alloc(0);
-	printf("%x\n", mem1);
-	uint64_t mem2 = buddy_alloc(0);
-	printf("%x\n", mem2);
-	uint64_t mem3 = buddy_alloc(0);
-	printf("%x\n", mem3);
-	buddy_free(mem1);
-	buddy_free(mem2);
-	buddy_free(mem3);*/
-	while (1);
+    get_memmap();
+    print_memmap();
+    /*uint64_t mem = buddy_alloc(0);
+    printf("%x\n", mem);
+    buddy_free(mem);
+    mem = buddy_alloc(1);
+    printf("%x\n", mem);
+    buddy_free(mem);
+    mem = buddy_alloc(2);
+    printf("%x\n", mem);
+    buddy_free(mem);
+    uint64_t mem1 = buddy_alloc(0);
+    printf("%x\n", mem1);
+    uint64_t mem2 = buddy_alloc(0);
+    printf("%x\n", mem2);
+    uint64_t mem3 = buddy_alloc(0);
+    printf("%x\n", mem3);
+    buddy_free(mem1);
+    buddy_free(mem2);
+    buddy_free(mem3);*/
+
+
+    struct slab_allocator * new_slab = init_slab(1024);
+    void * mem1 = alloc_slab(&new_slab);
+    void * mem2 = alloc_slab(&new_slab);
+    printf("%x %x\n", (uint64_t)mem1, (uint64_t)mem2);
+    free_slab(&new_slab, mem1);
+    free_slab(&new_slab, mem2);
+    mem1 = alloc_slab(&new_slab);
+    printf("%x\n", (uint64_t)mem1);
+
+
+    while (1);
 }
