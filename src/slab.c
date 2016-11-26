707d618c34c9bbf1ca2070809c342421f6752379
Hamelinny <anastasiyasofr@yandex.ru>
2016-11-26 21:44:03 +0300

    thread-safety

--- src/slab.c
+++ src/slab.c
@@ -1,6 +1,7 @@
 #include <slab.h>
 #include <buddy.h>
 #include <io.h>
+#include "lock.h"
 
 #define ALLOC_SIZE 16384
 #define nil (struct slab_allocator *)(-1)
@@ -23,18 +24,22 @@ struct slab_allocator * init_slab(size_t size) {
 }
 
 void * alloc_slab(struct slab_allocator ** slab) {
+	lock();
 	if ((*slab)->next == nil) {
 		struct slab_allocator * new_tail = init_slab((*slab)->size);
 		(*slab)->next = new_tail;
 	}
 	void * ans = (void *)(*slab);
 	(*slab) = (*slab)->next;
+	unlock();
 	return ans;
 }
 
 void free_slab(struct slab_allocator ** slab, void * mem) {
+	lock();
 	struct slab_allocator * new_head = (struct slab_allocator *)mem;
 	new_head->size = (*slab)->size;
 	new_head->next = (*slab);
 	(*slab) = new_head;
+	unlock();
 }
