10beec317d40804078be9f9d288318e1e5f87e06
Hamelinny <anastasiyasofr@yandex.ru>
2016-11-26 19:39:27 +0300

    creating threads and simple control

---
+++ inc/threads.h
@@ -0,0 +1,26 @@
+#ifndef __THREADS__
+#define __THREADS__
+
+#include <sys/types.h>
+
+
+pid_t get_current_thread();
+
+void init_threads();
+
+pid_t create_thread(void (*fun)(void *), void *arg);
+
+void run(pid_t tid);
+
+void yield();
+
+void finish_thread();
+
+void free_thread(pid_t prev_thread);
+
+
+
+pid_t switch_threads(void ** old_sp, void * new_sp);
+
+
+#endif
