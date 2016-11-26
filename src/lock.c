d30179c2d730a6b05df193669d11a5b8e74d1ed8
Hamelinny <anastasiyasofr@yandex.ru>
2016-11-26 19:42:29 +0300

    lock and unlock added

---
+++ src/lock.c
@@ -0,0 +1,19 @@
+#include "lock.h"
+#include "threads.h"
+
+void lock(struct spinlock *lock)
+{
+    const uint16_t ticket = __sync_fetch_and_add(&lock->users, 1);
+
+    while (lock->ticket != ticket) {
+        __asm__ volatile ("" : : : "memory");
+        yield();
+    }
+    __sync_synchronize();
+}
+
+void unlock(struct spinlock *lock)
+{
+    __sync_synchronize();
+    __sync_add_and_fetch(&lock->ticket, 1);
+}
