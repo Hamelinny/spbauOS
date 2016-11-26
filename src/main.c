2c0f2df58c5939624956263490468a79f2d86160
Hamelinny <anastasiyasofr@yandex.ru>
2016-11-26 21:42:09 +0300

    test removed

--- src/main.c
+++ src/main.c
@@ -21,39 +21,6 @@ static void qemu_gdb_hang(void)
 #include <lock.h>
 
 extern uint64_t handler[];
-
-struct spinlock spinlock1;
-struct spinlock spinlock2;
-
-int val = 0;
-
-void lock1(void * arg __attribute__((unused))) {
-    printf("lock1\n");
-    lock(&spinlock1);
-    printf("in spinlock1 %d\n", val);
-    yield();
-    lock(&spinlock2);
-    printf("in spinlock2 %d\n", val);
-    val = 1;
-    unlock(&spinlock2);
-    printf("out of spinlock2 %d\n", val);
-    unlock(&spinlock1);
-    printf("out of spinlock1 %d\n", val);
-
-}
-
-void lock2(void * arg __attribute__((unused))) {
-    printf("lock2\n");
-    lock(&spinlock1);
-    printf("in spinlock1 %d\n", val);
-    lock(&spinlock2);
-    printf("in spinlock2 %d\n", val);
-    val = 2;
-    unlock(&spinlock2);
-    printf("out of spinlock2 %d\n", val);
-    unlock(&spinlock1);
-    printf("out of spinlock1 %d\n", val);
-}
     
 
 void main(void) {
@@ -78,10 +45,6 @@ void main(void) {
     print_memmap();
 
     init_threads();
-    create_thread(lock1, 0);
-    create_thread(lock2, 0);
-    yield();
-    yield();
 
     while (1);
 }
